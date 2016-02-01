8635578000abb551cc49233626132c03f9b99620
admin <admin>
2016-02-01 11:43:15 +0200

    first

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simpleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the simpleApp
+ */
+angular.module('simpleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simpleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the simpleApp
+ */
+angular.module('simpleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
